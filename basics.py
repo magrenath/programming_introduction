8751c861b3885ee61952fa462633ecaf35c7a0e4
unknown <nmagre@ua.pt>
2022-03-23 19:13:08 +0000

    Concept of variables and its types

---
+++ basics.py
@@ -0,0 +1,22 @@
+name, age = "Nathália", 21
+
+print(name)
+print(age)
+
+fullName = "Nathália Magre Guerra Pinto"
+full_name = "N Magre"
+
+brand = "Amigoscode"
+age = 2
+numbers = []
+pi = 3.14
+isAdult = True
+
+print(type(brand))
+print(type(age))
+print((type(numbers)))
+print((type(pi)))
+print((type(isAdult)))
+
+course: str = "Python"
+isCool: bool = True
